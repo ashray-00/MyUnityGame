3fd586227a73c24cbbc1a910b906a7bcebad5eef
ashray-00 <ashrayadhikari471@gmail.com>
2018-09-25 10:30:26 +0200

    games

---
+++ Box Shooter/Assets/Scripts/NextLevelButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelButton : MonoBehaviour {
+	void OnCollisionEnter(Collision newCollision){
+		if (newCollision.gameObject.tag == "Projectile") {
+			GameManager.gm.NextLevel();
+		}
+	}
+}
